4ce9af1ad0735c61c223816544a09a40fd866ba7
Saksham091 <krishnaagrwal4@gmail.com>
2025-03-30 13:50:09 +0530

    add DailyForecast component to display today's weather forecast

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import AirPollution from "./Components/AirPollution/AirPollution";
+import DailyForecast from "./Components/DailyForecast/DailyForecast";
 import Navbar from "./Components/Navbar";
 
 
@@ -10,6 +11,7 @@ export default function Home() {
     <div className="flex flex-col w-full">
       <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
         <AirPollution />
+        <DailyForecast/>
       </div>
     </div>
 
