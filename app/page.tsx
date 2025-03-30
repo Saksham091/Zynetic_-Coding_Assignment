9b04df02bab2f3f814ab7a02d689429a3f2310a9
Saksham091 <krishnaagrwal4@gmail.com>
2025-03-30 13:55:18 +0530

    add FiveDayForecast component to display a 5-day weather forecast

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,24 @@ import AirPollution from "./Components/AirPollution/AirPollution";
 import DailyForecast from "./Components/DailyForecast/DailyForecast";
 import FeelsLike from "./Components/FeelsLike/FeelsLike";
 import Navbar from "./Components/Navbar";
+import FiveDayForecast from "./Components/FiveDayForecast/FiveDayForecast";
 
 
 export default function Home() {
   return <main className="mx-[1rem] lg:mx-[2rem] xl:mx-[6rem] 2xl:mx-[16rem] m-auto">
     <Navbar />
-
-    <div className="flex flex-col w-full">
-      <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
-        <AirPollution />
-        <DailyForecast/>
-        <FeelsLike />
+    <div className="pb-4 flex flex-col gap-4 md:flex-row">
+      <div className="flex flex-col gap-4 w-full min-w-[18rem] md:w-[35rem]">
+        <FiveDayForecast />
+      </div>
+      <div className="flex flex-col w-full">
+        <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
+          <AirPollution/>
+          <DailyForecast/>
+          <FeelsLike />
+        </div>
       </div>
     </div>
-
-
     <footer className="py-4 flex justify-center pb-8">
       <p className="footer-text text-sm flex items-center gap-1">
         Made By
